9ea381642e6b86f9a3646ce17da51233909ce00f
CESAR VILLALOBOS <cesarv.h@berkeley.edu>
2015-10-10 17:27:35 -0700

    settings starter

--- RadiusApp/app/src/main/java/io/github/reginax/radiusapp/MainActivity.java
+++ RadiusApp/app/src/main/java/io/github/reginax/radiusapp/MainActivity.java
@@ -1,10 +1,13 @@
 package io.github.reginax.radiusapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
     // Displays the messages receieved
     public void displayReceived(View view) {
 
-    }
 
+    }
     // Displays the messages sent
     public void displaySent(View view) {
 
@@ -29,12 +32,17 @@ public class MainActivity extends AppCompatActivity {
 
     // prep and send a new message
     public void sendNewMsg(View view) {
-
+//        Intent intent = new Intent(this, SendNewMessageActivity.class);
+//        Button buttonPressed = (EditText) findViewById(R.id.button);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
     }
 
     // display the current settings
     public void displaySettings(View view) {
-
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 
     // show a list of friends
